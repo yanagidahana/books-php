297ead18a979a44dd5ef45b47877d95e56f4d839
yanagidahana <hana878787zzz@gmail.com>
2021-01-21 16:59:21 +0900

    リンクの修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>書籍リスト</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<title>書籍リスト</title>
 
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet" type="text/css">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -13,11 +14,29 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
+	<style>
+		body {
+			font-family: 'Raleway';
+			margin-top: 25px;
+		}
+
+		button .fa {
+			margin-right: 6px;
+		}
+
+		.table-text div {
+			padding-top: 6px;
+		}
+	</style>
+
+	<script>
+		$(function () {
+			$('#book-name').focus();
+		});
+	</script>
 </head>
-
-
 <body>
-<div class="container">
+	<div class="container">
 		<nav class="navbar navbar-default">
 			<div class="container-fluid">
 				<div class="navbar-header">
@@ -40,6 +59,5 @@
 		</nav>
 	</div>
 	@yield('content')
-
 </body>
 </html>
